534299ae828e9dac044ecc28bcef6e5dcb35aa28
Amit Kumar <apraka008c@cable.comcast.com>
2018-10-19 00:18:40 -0400

    display player info and modal windo to create new player

--- resources/js/components/players.vue
+++ resources/js/components/players.vue
@@ -1,16 +1,119 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Players Component</div>
+        <div class="row mt-5">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Players Information</h3>
 
-                    <div class="card-body">
-                        I'm in Players component.
-                    </div>
+                <div class="card-tools">
+                  <button class="btn btn-success" data-toggle="modal" data-target="#addNewModal">Add New 
+                      <i class="fas fa-user-plus fa-fw"></i></button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover">
+                  <tbody><tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Type</th>
+                    <th>More Info</th>
+                    <th>Modify</th>
+                  </tr>
+                  <tr>
+                    <td>183</td>
+                    <td>John Doe</td>
+                    <td>11-7-2014</td>
+                    <td><span class="tag tag-success">Approved</span></td>
+                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    <td>
+                      <a href="#">
+                          <i class="fa fa-edit"></i>
+                      </a>
+                      &nbsp;/&nbsp;
+                      <a href="#">
+                          <i class="fa fa-trash red"></i>
+                      </a>
+                  </td>
+                  </tr>
+                  <tr>
+                    <td>219</td>
+                    <td>Alexander Pierce</td>
+                    <td>11-7-2014</td>
+                    <td><span class="tag tag-warning">Pending</span></td>
+                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    <td>
+                      <a href="#">
+                          <i class="fa fa-edit"></i>
+                      </a>
+                      &nbsp;/&nbsp;
+                      <a href="#">
+                          <i class="fa fa-trash red"></i>
+                      </a>
+                  </td>
+                  </tr>
+                  <tr>
+                    <td>657</td>
+                    <td>Bob Doe</td>
+                    <td>11-7-2014</td>
+                    <td><span class="tag tag-primary">Approved</span></td>
+                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    <td>
+                      <a href="#">
+                          <i class="fa fa-edit"></i>
+                      </a>
+                      &nbsp;/&nbsp;
+                      <a href="#">
+                          <i class="fa fa-trash red"></i>
+                      </a>
+                  </td>
+                  </tr>
+                  <tr>
+                    <td>175</td>
+                    <td>Mike Doe</td>
+                    <td>11-7-2014</td>
+                    <td><span class="tag tag-danger">Denied</span></td>
+                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    <td>
+                      <a href="#">
+                          <i class="fa fa-edit"></i>
+                      </a>
+                      &nbsp;/&nbsp;
+                      <a href="#">
+                          <i class="fa fa-trash red"></i>
+                      </a>
+                  </td>
+                  </tr>
+                </tbody></table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+
+        <div class="modal fade" id="addNewModal" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="addNewLabel">Add New</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Create</button>
+                </div>
                 </div>
             </div>
         </div>
+
     </div>
 </template>
 
