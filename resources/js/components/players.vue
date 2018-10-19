c9d2a4f71f8816f35ac0108941ad8ca85fa29923
Amit Kumar <apraka008c@cable.comcast.com>
2018-10-19 01:12:37 -0400

    Updated player component to validate the form for add new

--- resources/js/components/players.vue
+++ resources/js/components/players.vue
@@ -103,13 +103,45 @@
                     <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
+                <form @submit.prevent="createUser">
                 <div class="modal-body">
-                    ...
+                    <div class="form-group">
+                        <input v-model="form.name" type="text" name="name"
+                            placeholder="Name"
+                            class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                        <has-error :form="form" field="name"></has-error>
+                    </div>
+                
+                    <div class="form-group">
+                        <input v-model="form.email" type="email" name="email"
+                            placeholder="Email Address"
+                            class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                        <has-error :form="form" field="email"></has-error>
+                    </div>
+                
+                    <div class="form-group">
+                        <select name="type" v-model="form.type" id="type" 
+                        class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                            <option value="">Select User Role</option>
+                            <option value="admin">Admin</option>
+                            <option value="player">Standard Player</option>
+                            <option value="organizer">Organizer</option>
+                        </select>    
+                    </div>
+                
+                    <div class="form-group">
+                        <input v-model="form.password" type="password" name="password" id="password"
+                            placeholder="Password"
+                            class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                        <has-error :form="form" field="password"></has-error>
+                    </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                     <button type="button" class="btn btn-primary">Create</button>
                 </div>
+
+                </form>
                 </div>
             </div>
         </div>
@@ -119,6 +151,20 @@
 
 <script>
     export default {
+        data() {
+            return {
+                form: new Form({
+                    name: '',
+                    email : '',
+                    password : ''
+            })
+        }
+        },
+        methods: {
+            createUser(){
+                this.form.post('api/user')
+            }
+        },
         mounted() {
             console.log('Component mounted.')
         }
