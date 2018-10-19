77134544c082e6f0ff52727c7f0f7c868b3692e9
Amit Kumar <apraka008c@cable.comcast.com>
2018-10-18 23:19:55 -0400

    Custom colors for icons and logout functionality

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -170,7 +170,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
           <li class="nav-item">
             <router-link to="/dashboard" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <i class="nav-icon fas fa-tachometer-alt blue"></i>
               <p>
                 Dashboard
               </p>
@@ -178,7 +178,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-cog"></i>
+              <i class="nav-icon fas fa-cog green"></i>
               <p>
                 Management
                 <i class="right fas fa-angle-left"></i>
@@ -187,7 +187,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <ul class="nav nav-treeview">
               <li class="nav-item">
                 <router-link to="/dashboard" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                  <i class="far fa-circle nav-icon purple"></i>
                   <p>Active Page</p>
                 </router-link>
               </li>
@@ -201,19 +201,26 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
           <li class="nav-item">
             <router-link to="/profile" class="nav-link">
-              <i class="nav-icon fas fa-user"></i>
+              <i class="nav-icon fas fa-user orange"></i>
               <p>
                 Profile
               </p>
             </router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/logout" class="nav-link">
-              <i class="nav-icon fa fa-power-off"></i>
-              <p>
-                Logout
-              </p>
-            </router-link>
+            <a class="nav-link" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                              document.getElementById('logout-form').submit();">
+                  <i class="nav-icon fa fa-power-off red"></i>
+                  <p>
+                    {{ __('Logout') }}  
+                  </p>
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+
           </li>
           
 
@@ -221,7 +228,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <hr>
           <li class="nav-item">
             <router-link to="/home" class="nav-link">
-              <i class="nav-icon fas fa-user"></i>
+              <i class="nav-icon fas fa-home blue"></i>
               <p>
                 Home
               </p>
@@ -229,7 +236,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
           <li class="nav-item">
             <router-link to="/players" class="nav-link">
-              <i class="nav-icon fas fa-user"></i>
+              <i class="nav-icon fas fa-users green"></i>
               <p>
                 Players
               </p>
@@ -238,7 +245,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
           <li class="nav-item">
             <router-link to="/rules" class="nav-link">
-              <i class="nav-icon fas fa-user"></i>
+              <i class="nav-icon fas fa-edit orange"></i>
               <p>
                 Rules
               </p>
@@ -247,7 +254,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
           <li class="nav-item">
             <router-link to="/gallery" class="nav-link">
-              <i class="nav-icon fas fa-user"></i>
+              <i class="nav-icon fas fa-camera-retro red"></i>
               <p>
                 Gallery
               </p>
@@ -256,7 +263,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
           <li class="nav-item">
             <router-link to="/recordmatch" class="nav-link">
-              <i class="nav-icon fas fa-user"></i>
+              <i class="nav-icon fas fa-hand-point-right indigo"></i>
               <p>
                 Record Match
               </p>
@@ -264,7 +271,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
           <li class="nav-item">
             <router-link to="/standings" class="nav-link">
-              <i class="nav-icon fas fa-user"></i>
+              <i class="nav-icon fas fa-list-ol yellow"></i>
               <p>
                 Standings
                 <span class="right badge badge-danger">New</span>
